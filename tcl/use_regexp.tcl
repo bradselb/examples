530680aae4f1f5c10c0d74c34b7409fc3461bee7
bradselb <Brad.Selbrede@yahoo.com>
2014-11-09 11:53:07 -0800

    minor refoctoring and output more info.

--- tcl/use_regexp.tcl
+++ tcl/use_regexp.tcl
@@ -4,9 +4,12 @@ set tclver [info tcl]
 puts "tcl version $tclver"
 
 set reply {<h1 class="ip">Your <acronym title="Internet Protocol">IP</acronym> Address is: <span>67.108.13.66</span></h1>}
-puts "$reply"
+#puts "$reply"
 
-#set matchCount [regexp -nocase -expanded {(?:address) (?:[\s\w:\<]+[\>]) ([\d]+[\.][\d]+[\.][\d]+[\.][\d]+)}  $reply all addr]
-set matchCount [regexp -nocase -expanded {([\d]+[\.][\d]+[\.][\d]+[\.][\d]+)}  $reply all addr]
-puts "match count: $matchCount, all: '$all', address: '$addr'"
+set expression {([\d]+[\.][\d]+[\.][\d]+[\.][\d]+)}
+set matchCount [regexp -nocase -expanded $expression $reply all addr]
+
+puts "match count: $matchCount"
+puts "all: '$all'"
+puts "address: '$addr'"
 
