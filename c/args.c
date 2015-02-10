85dbbe1da91cd94c4f575612c8cd89c894f6e44b
bradselb <Brad.Selbrede@yahoo.com>
2015-02-09 19:59:41 -0800

    add another way to iterate over cmd line args.

--- c/args.c
+++ c/args.c
@@ -12,27 +12,45 @@ int main(int argc, char* argv[])
     size_t size;
     size_t totsize = 0;
 
-    i = 0;
-    while (argv[i]) {
-	printf("argv[%d] is: %p -- > '%s'\n", i, argv[i], argv[i]);
-	++i;
+    // iterate over all the cmd line args, printing each one to stdout. 
+    for (i=0; i<argc; ++i) {
+        size = strlen(argv[i]) + 1;
+        totsize += size;
+        printf("argv[%d] is: %p -- > '%s'\t", i, argv[i], argv[i]);
+        printf ("\t%p + %lu is: %p\n", argv[i], size, (argv[i] + size));
     }
+
 
     // another way...
-    for (i=0; i<argc; ++i) {
-	size = strlen(argv[i]) + 1;
-	totsize += size;
-	printf("argv[%d] is: %p -- > '%s'\t", i, argv[i], argv[i]);
-	printf ("\t%p + %lu is: %p\n", argv[i], size, (argv[i] + size));
+    i = 0;
+    while (argv[i]) {
+        printf("argv[%d] is: %p -- > '%s'\n", i, argv[i], argv[i]);
+        ++i;
     }
 
+    // still another method of iterating over the cmd line args. 
+    // this again makes use of the fact that the last string in argv is 
+    // a zero byte.
+    char** argp = argv;
+    while (*argp) {
+        puts(*argp);
+        ++argp;
+    }
+
+
+
+    // demonstrate that all of the striings in argv are in one buffer...
+    // as if the shell used strtok() to parse the cmd line args. 
+    // this does assume that which it is trying to show.
+    // walk through the whole buffer replacing null bytes with a colon
+    // and printing each character to stdout. 
     char* p = argv[0];
     char c;
     for (i=0; i<totsize; ++i) {
-	c = *p;
-	if (0 == c) c = ':';
-	putchar(c);
-	++p;
+        c = *p;
+        if (0 == c) c = ':';
+        putchar(c);
+        ++p;
     }
     putchar('\n');
 
