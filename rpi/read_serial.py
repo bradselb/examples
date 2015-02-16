be9bd73a6a2695402ee5bef698a08cb3f13cb14d
bradselb <bks62464@yahoo.com>
2015-02-16 14:31:00 -0800

    write to file named on cmd line.

--- rpi/read_serial.py
+++ rpi/read_serial.py
@@ -1,24 +1,37 @@
 #! /usr/bin/python
 import serial 
-import time 
+import time
+import sys
+import os
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = '%s.%s' % (time.strftime('%Y%m%d'), 'nmea')
+
+if not os.path.exists(filename):
+    f = open(filename, "w")
+    f.close()
 
 while True:
-    port = serial.Serial(port="/dev/ttyAMA0", baudrate=9600)
+    with serial.Serial(port="/dev/ttyAMA0", baudrate=9600) as port:
+        with open(filename, "a") as f:
 
-    line_count = 0
-    line = ""
-    while line_count < 10:
-        ch = port.read(1)
-
-        if (ch != '\r' and ch != '\n'):
-            line += ch
-
-        if (ch == '\n'):
-            if (0 == line.find("$GPGGA")) or (0 == line.find("$GPRMC")):
-                print(line)
-                line_count += 1
+            line_count = 0
             line = ""
+            while line_count < 10:
+                ch = port.read(1)
 
-    port.close()
+                if (ch != '\r'):
+                    line += ch
+
+                if (ch == '\n'):
+                    if (0 == line.find("$GPGGA")) or (0 == line.find("$GPRMC")):
+                        print(line)
+                        f.write(line)
+                        line_count += 1
+                    line = ""
+
+
     time.sleep(30)
 
