54594fc453a7f205d520a1646c53a88010a24769
BradSelb <bks62464@yahoo.com>
2016-09-23 10:29:29 -0700

    need to put socket in a class with signals and slots

--- Qt/client/main.cpp
+++ Qt/client/main.cpp
@@ -7,12 +7,13 @@ int main(int argc, char** argv)
     int rc;
     QCoreApplication app(argc, argv);
     QStringList args(app.arguments());
+
     QStringList::const_iterator iter;
     for (iter=args.begin(); iter!=args.end(); ++iter) {
         printf("%s\n", iter->toAscii().data());
     }
 
-    QTcpSocket sock;
+    // QTcpSocket sock;
 
     rc = app.exec();
     return rc;
