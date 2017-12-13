2b9bfc06335b14b76c1b41bdb4f5ebef9dac8542
bradselb <brads9524@gmail.com>
2017-12-13 05:21:12 -0800

    add support for a couple of command line args

--- Qt/client/main.cpp
+++ Qt/client/main.cpp
@@ -4,21 +4,38 @@
 
 int main(int argc, char** argv)
 {
-    int rc;
+    int rc = -1;
     QCoreApplication app(argc, argv);
 
     Client* client = new Client(&app);
     QObject::connect(client, SIGNAL(bailout()), &app, SLOT(quit()));
 
+    // get host name from command line args and, optionally, the port number too.
+    QString host;
+    QString port("1735");
     QStringList args(app.arguments());
     QStringList::const_iterator iter;
     for (iter=args.begin(); iter!=args.end(); ++iter) {
-        printf("%s\n", iter->toAscii().data());
+        QString hostCmdLineSw("--host");
+        QString portCmdLineSw("--port");
+        if (iter->startsWith(hostCmdLineSw)) {
+            //  --host=example.domain.com
+            host = iter->split("=").at(1);
+        } else if (iter->startsWith(portCmdLineSw)) {
+            //  --port=1735
+            port = iter->split("=").at(1);
+        }
+        //printf("%s\n", iter->toAscii().data());
     }
 
-    // TODO: get these from commmand line switches. 
-    client->connectToHost(QString("harpina"), 1735);
+    if (host.isEmpty()) {
+        // print a helpful usage message and exit with an error code.
+        rc = -1;
+    } else {
+        // display an informative success message and start the event loop.
+        client->connectToHost(host, port.toInt());
+        rc = app.exec();
+    }
 
-    rc = app.exec();
     return rc;
 }
