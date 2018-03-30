282bf293a845acbb0a24eac620f629c36346da4d
Brad Selbrede <bks62464@yahoo.com>
2018-03-30 13:15:34 -0700

    minor clean up and simplification

--- python/echo-client/client.py
+++ python/echo-client/client.py
@@ -1,37 +1,27 @@
+#! /usr/bin/python
+
 import socket
-
-
 class TestClient:
 	def __init__(self, target_addr, tcp_port=7000, my_ip_addr=0, *args, **kwargs):
 		self.target_addr = (target_addr, tcp_port)
 		self.source_addr = (my_ip_addr, 0)
-
-
 	def sendCommandLine(self, cmdline):
 		"""
 		This function connected to a TCP Server runnin gon the target and 
 		together, this client-server pair act like a very primative remote
 		shell. 
-
 		Send the command line to the target and receive whatever is written to 
 		stdout by the remote process.  
 		"""
-
 		# TODO: barf if fed garbage.
-
 		sock = socket.create_connection(self.target_addr, timeout=10)
-
 		sock.sendall(cmdline)
 		response = sock.recv(4096)
 		sock.shutdown(socket.SHUT_RDWR)
 		sock.close()
-
 		return response
-
 if __name__ == '__main__' :
-    myClient = TestClient('localhost')
+    myClient = TestClient('saturn')
     for cmd in ('uname -nrv', 'ls -l', 'who', 'cal'):
         s = myClient.sendCommandLine("%s\n" % (cmd))
         print s
-
-
