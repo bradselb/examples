e13726407d699880b782b5728674178e5d90a1fd
bradselb <bks62464@yahoo.com>
2022-04-14 12:43:12 -0700

    add a function to verify the checksum.

---
+++ Qt/gps/checksum.h
@@ -0,0 +1,15 @@
+#if !defined VERIFY_CHECKSUM_H
+#define VERIFY_CHECKSUM_H
+
+#ifdef __cplusplus 
+extern "C" {
+#endif 
+
+int verify_checksum(const char*);
+
+#ifdef __cplusplus 
+}
+#endif 
+
+
+#endif //!defined VERIFY_CHECKSUM_H
